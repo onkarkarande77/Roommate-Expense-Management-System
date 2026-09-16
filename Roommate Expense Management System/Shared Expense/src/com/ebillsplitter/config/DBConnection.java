59f69e525e2c4d58dd5aa619297ee28a883443bb
onkarkarande77 <karandeonkar05@gmail.com>
2026-09-16 20:49:20 +0530

    Update DBConnection.java

--- Roommate Expense Management System/Shared Expense/src/com/ebillsplitter/config/DBConnection.java
+++ Roommate Expense Management System/Shared Expense/src/com/ebillsplitter/config/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
 	private static final String URL="jdbc:mysql://localhost:3306/ebillsplitter";
     private static final String USER="root";
-    private static final String PASSWORD="pass@123"; 
+    private static final String PASSWORD="pass@123";
     
     public static Connection getConnection() throws Exception{
     	
